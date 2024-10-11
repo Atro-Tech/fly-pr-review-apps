6472be1bd7af516d470bfdf506238b1fe6c61b9f
Philipp <github@vanbittern.com>
2024-10-12 01:07:38 +0200

    --yes!

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,7 +58,7 @@ if [ -n "$INPUT_CERT_HOSTNAME" ]; then
   fi
 fi
 
-fly scale --app "$app" count 1
+fly scale --app "$app" --yes count 1
 
 # Make some info available to the GitHub workflow.
 fly status --app "$app" --json >status.json
